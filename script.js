7d1ebc221811ba51aee4d93811564e2d4b611ca8
Eddy <154628904+Just-404@users.noreply.github.com>
2024-07-26 18:29:28 -0300

    Add winning logic
    Added to GameController some methods that check if one player wins by row,
    column, diagonal or anti-diagonal.
    
    Also added a counter that that increases per round played. If the board
    is filled and no one has won on 9 turns, it is considered a draw.

--- script.js
+++ script.js
@@ -81,20 +81,133 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
     const displayRound = () => {
         board.printBoard();
         console.log(`\nIt\'s ${getActivePlayer().name} turn.`);
+        console.log("Placing token...");        
+
+
     }
+
+    const checkWinning = () => {
+        const player1 = players[0].token;
+        const player2 = players[1].token;
+        const boardArr = board.getBoard().map((row) => row.map((cell) => cell.getPlayerSelection()));
+        const winningArrayPlayer1 = [player1, player1, player1];
+        const winningArrayPlayer2 = [player2, player2, player2]
+
+        const checkRowWin = (arr, playerArr) => arr.find((row) => row.every((elem, idx) => elem === playerArr[idx]));
+        
+        const checkColWin = (arr, playerArr) => {
+
+            for(let i=0; i<3; i++){
+                let col = [];
+
+                for (let j = 0; j < 3; j++) {
+                    col.push(arr[j][i]);                    
+                }
+
+                if(col.every((elem, idx) => elem === playerArr[idx])){
+                    return true;
+                };
+            }        
+          
+        }
+
+        const checkDiagWin = (arr, playerArr) => {
+            let diag = [];
+
+            for(let i=0; i<3; i++){
+                diag.push(arr[i][i]);                                
+
+            }  
+            if(diag.every((elem, idx) => elem === playerArr[idx])){
+                return true;
+            };
+        }
+
+        const checkAntiDiagWin = (arr, playerArr) => {
+          
+            let antiDiag = [];      
+
+            for(let i=0; i<3; i++){
+
+                for(let j=2; j>=0; j--){
+                    if(i+j == 2) antiDiag.push(arr[i][j]);
+                }
+            }
+            if(antiDiag.every((elem, idx) => elem === playerArr[idx])){
+                return true;
+            };
+        }
+
+        const wonBy = ()=>{
+            if(checkRowWin(boardArr, winningArrayPlayer1) || checkRowWin(boardArr, winningArrayPlayer2)) {
+                return "wins by row.";
+            }
+            else if(checkColWin(boardArr, winningArrayPlayer1) || checkColWin(boardArr, winningArrayPlayer2)){
+                return "wins by column.";
+            }
+            else if(checkDiagWin(boardArr, winningArrayPlayer1) || checkDiagWin(boardArr, winningArrayPlayer2)){
+                return "wins by diagonal.";
+            }
+            else if(checkAntiDiagWin(boardArr, winningArrayPlayer1) || checkAntiDiagWin(boardArr, winningArrayPlayer2)){
+                return "wins by anti-diagonal.";
+            }
+           
+        }
+      
+        return wonBy();
+
+    }
+    const gameOver = (winMsg) => {
+        if(roundsPlayed <= 9){
+            board.printBoard();
+
+            if(winMsg == "draw") {
+                console.log("It's a draw!")
+            }
+            else console.log(`${getActivePlayer().name} ${winMsg}`);
+            
+        }
+
+      
+    }
+    let roundsPlayed = 0;
 
     const playRound = (cellRow, cellCol) => {
 
        let value = board.placeToken(getActivePlayer().token, cellRow, cellCol);
 
-       if(value == "Cell is not empty"){
-        console.log("Cell is not empty. Try again\n");
+       if(value == "Cell is not empty" && roundsPlayed < 9){
+             console.log("Cell is not empty. Try again\n");
+             displayRound();
        }
        else{
-        console.log("Placing token...");
+            roundsPlayed++;
+            if(roundsPlayed < 9) {
+                
+                let winMsg;
 
-        switchPlayerTurn();
-        displayRound();
+                if(roundsPlayed >= 3){
+                    winMsg= checkWinning();
+                }
+
+                if(winMsg) {
+  
+                    gameOver(winMsg);
+                }
+                else{
+    
+                    switchPlayerTurn();
+                    displayRound();
+    
+                }
+             
+            }
+            else{
+                gameOver("draw");
+                
+            }
+           
+          
        }
        
     }
@@ -104,10 +217,16 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
     return{ getActivePlayer, playRound, }
 }
 
+// test game
 const game = GameController();
 game.playRound(1, 2);
-game.playRound(1, 2);
+game.playRound(2, 0);
+game.playRound(2, 1);
+game.playRound(2, 2)
 game.playRound(0, 2);
-game.playRound(1, 2);
-game.playRound(0, 2)
+game.playRound(0, 1);
+game.playRound(0, 0);
+game.playRound(1, 1)
 game.playRound(1, 0);
+game.playRound(1, 0);
+
