e22fca41e74ada54ef5efe16fc0814b87941a5dd
Eddy <154628904+Just-404@users.noreply.github.com>
2024-07-29 20:03:50 -0300

    Link DOM elements with the game logic
    Added a ScreenController function that is in charge of displaying the
    the game state. First of all, a modal is opened when the page loads,
    it takes the player's names input and display them on screen along with
    the board. Every time a "cell" of the board is clicked it shows the
    player token until a winning or tie condition is met. Finally it opens a
    modal that shows the "winning state".
    
    Also, added clearBoard functions to GameBoard and GameController, which
    are in charge of "resetting" the game.

--- script.js
+++ script.js
@@ -32,6 +32,9 @@ function GameBoard(){
         }
     }
 
+    const clearBoard = () => {
+        board.forEach((row) => row.forEach((cell) => cell.playerSelection(0)));
+    }
     const getBoard = () => board;
    
     const placeToken = (playerSel, cellRow, cellCol) => {
@@ -51,7 +54,7 @@ function GameBoard(){
         getBoard,
         placeToken,
         printBoard,
-
+        clearBoard,
     }
 }
 
@@ -162,15 +165,30 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
             board.printBoard();
 
             if(winMsg == "draw") {
-                console.log("It's a draw!")
+                console.log("It's a draw!");
+                winningMsg = "It's a draw!";
             }
-            else console.log(`${getActivePlayer().name} ${winMsg}`);
+            else {
+                console.log(`${getActivePlayer().name} ${winMsg}`);
+                winningMsg = `${getActivePlayer().name} ${winMsg}`;
+            }
             
         }
 
       
     }
+
+    const resetGame = () => {
+        board.clearBoard();
+        activePlayer = players[0];
+        winningMsg = "";
+        roundsPlayed = 0
+    }
+
     let roundsPlayed = 0;
+    let winningMsg = "";
+
+    const getWinMsg = () => winningMsg;
 
     const playRound = (cellRow, cellCol) => {
 
@@ -183,11 +201,10 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
        else{
             roundsPlayed++;
             if(roundsPlayed < 9) {
-                
                 let winMsg;
 
                 if(roundsPlayed >= 3){
-                    winMsg= checkWinning();
+                    winMsg = checkWinning();
                 }
 
                 if(winMsg) {
@@ -195,7 +212,6 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
                     gameOver(winMsg);
                 }
                 else{
-    
                     switchPlayerTurn();
                     displayRound();
     
@@ -212,21 +228,149 @@ function GameController(playerOneName = "Player One", playerTwoName = "Player Tw
        
     }
 
-    displayRound();
-
-    return{ getActivePlayer, playRound, }
+    return{ getActivePlayer, playRound, getBoard: board.getBoard, getWinMsg, resetGame}
 }
 
-// test game
-const game = GameController();
-game.playRound(1, 2);
-game.playRound(2, 0);
-game.playRound(2, 1);
-game.playRound(2, 2)
-game.playRound(0, 2);
-game.playRound(0, 1);
-game.playRound(0, 0);
-game.playRound(1, 1)
-game.playRound(1, 0);
-game.playRound(1, 0);
 
+function ScreenController() {
+
+    let game = GameController();
+    let players = [];
+    const boardDiv = document.querySelector(".board");
+    const clearBtn = document.querySelector("#clear-btn");
+    let winMsg = "";
+
+
+    const openModal = (element, event) => {
+        document.addEventListener(event, () =>{
+            element.showModal();
+        });
+    }
+
+    const closeModal = (element) => {
+        document.addEventListener("click", () =>{
+            element.close();
+        });
+    }
+
+   
+    const showPlayerScreen = () => {
+        const dialog = document.getElementById("start-game");
+        const submitBtn = dialog.querySelector("#get-names");
+
+        openModal(dialog, "DOMContentLoaded");
+
+        submitBtn.addEventListener("click", (event) =>{
+            event.preventDefault();
+            let playerOneName = dialog.querySelector("#player-one").value;
+            let playerTwoName = dialog.querySelector("#player-two").value;
+
+            players = [playerOneName, playerTwoName];
+            closeModal(dialog);
+            setNames();
+            updateScreen();
+        })    
+    }
+
+    const showEndGameScreen = (msg) => {
+        const dialog = document.getElementById("gameover");
+        const playAgainBtn = dialog.querySelector(".container button");
+        const output = dialog.querySelector(".container output");
+        const retBtn = dialog.querySelector("#return-btn");
+
+        dialog.showModal();
+        output.textContent = msg;
+
+        playAgainBtn.addEventListener("click", () => {
+            clearBoard();
+            dialog.close();
+           
+
+        });
+
+        retBtn.addEventListener("click", () => dialog.close());
+
+    }
+
+    const clearBoard = () => {
+        game.resetGame();
+        winMsg = "";
+        updateScreen();
+    }
+
+    const checkNames = () => {
+        if(players[0] === "") players[0] = "Player One";
+        if(players[1] === "") players[1] = "Player Two";
+    }
+
+    const setNames = () => {
+        const playerOneNameDiv = document.querySelector(".player1");
+        const playerTwoNameDiv = document.querySelector(".player2");
+        checkNames();
+
+        playerOneNameDiv.textContent += players[0];
+        playerTwoNameDiv.textContent += players[1];
+      
+
+        game = GameController(players[0], players[1]);
+
+    }
+
+    const updateScreen = () => {
+        boardDiv.textContent = "";
+
+        const board = game.getBoard();
+
+        board.forEach((row, rowIdx) => {
+            row.forEach((elem, colIdx) => {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+
+                cell.dataset.rowIdx = rowIdx;
+                cell.dataset.colIdx = colIdx;
+
+                let cellValue = elem.getPlayerSelection();
+
+                if(cellValue === 0){
+                    cell.textContent = "";
+                }
+                else if(cellValue === 1){
+                    cell.textContent = "O";
+
+                }
+                else{
+                    cell.textContent = "X";
+
+                }
+                boardDiv.appendChild(cell);
+
+            });            
+        });
+
+    
+    }
+
+    const clickBoard = (e) => {
+        const rowIdx = e.target.dataset.rowIdx;
+        const colIdx = e.target.dataset.colIdx;
+
+        game.playRound(rowIdx, colIdx);
+        winMsg = game.getWinMsg();
+        console.log(winMsg != "");
+
+        if(winMsg != "") showEndGameScreen(winMsg);
+        updateScreen();
+
+    }
+
+    const startGame = () => {
+        showPlayerScreen();
+        boardDiv.addEventListener("click", clickBoard);
+        clearBtn.addEventListener("click", clearBoard);
+        
+    }
+    
+    startGame();
+}
+
+ScreenController();
